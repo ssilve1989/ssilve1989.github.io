5a86afb87ad46e2ae0d2886eeee21a237ef14fec
Steven Silvestri <steven.m.silvestri@gmail.com>
2017-09-16 16:34:43 -0400

    homepage done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Spotlight from './components/Spotlight';
 import Navigation from './components/Navigation';
 import About from './components/About';
 import Experience from './components/Experience';
-import Skills from './components/Skills/index';
+import Skills from './components/Skills';
 import 'hover.css/css/hover.css'
 import './index.css';
 import './App.css'
@@ -39,10 +39,62 @@ class App extends Component {
 				         rel="noopener noreferrer"
 				/>
 				<Navigation className="nav-area"/>
-				<About className="about-me-area" />
-				<Spotlight className="spotlight-area bg-green" />
-				<Experience className="exp-area bg-purple" />
-				<Skills className="skills-area bg-blue" />
+				<About className="about-me-area"/>
+				<Spotlight className="spotlight-area bg-green"/>
+				<Experience className="exp-area bg-purple"/>
+				<Skills className="skills-area bg-blue"/>
+				<footer className="footer-area row">
+					<div className="col-md-4">
+						<h4>Steve Silvestri</h4>
+						<p>Software Engineer, JavaScript Developer</p>
+						<small>
+							&copy; Steven Silvestri 2017
+						</small>
+					</div>
+					<div className="col-md-4">
+						<h4>Download CV</h4>
+						<p>
+							<a
+								href="https://drive.google.com/open?id=0B-QjQxm8doHNeW0xUWVjWUE0V1E" target="_blank">
+								<i className="fa fa-arrow-circle-o-down"/>&nbsp; Download</a>
+						</p>
+					</div>
+					<div className="col-md-4">
+						<h4>Follow me at</h4>
+						<ul className="SocialMedia">
+							<li className="social-icon">
+								<a href="https://www.github.com/ssilve1989">
+									<i className="fa fa-2x fa-github"/>
+								</a>
+							</li>
+							<li className="social-icon">
+								<a href="http://www.facebook.com/steve.silvestri.33">
+									<i className="fa fa-2x fa-facebook-square"/>
+								</a>
+							</li>
+							<li className="social-icon">
+								<a href="https://www.twitter.com/jsx1080">
+									<i className="fa fa-2x fa-twitter"/>
+								</a>
+							</li>
+							<li className="social-icon">
+								<a href="http://www.instagram.com/jsx1080">
+									<i className="fa fa-2x fa-instagram"/>
+								</a>
+							</li>
+							<li className="social-icon">
+								<a href="http://www.linkedin.com/in/stevensilvestri">
+									<i className="fa fa-2x fa-linkedin"/>
+								</a>
+							</li>
+							<li className="social-icon">
+								<a href="http://www.soundcloud.com/stevesilvestri">
+									<i className="fa fa-2x fa-soundcloud"/>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</footer>
 			</div>
 		)
 	}
