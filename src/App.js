2ab77f9ed39805946acf02a790bfe88c8d66fae0
Steven Silvestri <steven.m.silvestri@gmail.com>
2017-10-10 09:42:13 -0400

    resume only branch, for print

--- src/App.js
+++ src/App.js
@@ -1,129 +1,19 @@
 import React, { Component } from 'react'
-import TextBox from './components/TextBox'
-import logo from './img/html5Logo.png'
-import IconBox from './components/IconBox'
-import TextScroller from './components/TextScroller'
-import Home from './routes/Home';
 import Resume from './routes/Resume';
-import Navigation from './components/Navigation';
-import Career from './routes/Career';
 import 'hover.css/css/hover.css'
 import './index.css';
 import './App.css'
-import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="GridContainer">
-				<div className="logo-area">
-					<img className="img-fluid Logo" src={ logo } alt=""/>
-				</div>
-				<TextBox className="HeaderBox title-area bg-gray"
-				         value={ <h2>Steven Silvestri</h2> }
-				/>
-				<IconBox className="download-area hvr-sweep-to-right bg-light-gray"
-				         icon="fa fa-4x fa-download"
-				         to="https://drive.google.com/open?id=0B-QjQxm8doHNeW0xUWVjWUE0V1E"
-				         target="_blank"
-				         title="Download Resume PDF"
-				         rel="noopener noreferrer"
-				         external
-				/>
-				<TextBox className="HeaderBox position-area bg-light-gray"
-				         value={ this.getTextScroller() }
-				/>
-				<IconBox className="linkedin-area hvr-sweep-to-right bg-gray"
-				         icon="fa fa-4x fa-linkedin"
-				         to="https://www.linkedin.com/in/stevensilvestri"
-				         target="_blank"
-				         rel="noopener noreferrer"
-				         external
-				/>
-				<Navigation className="nav-area"/>
-				{ this.renderRoutes() }
-				<footer className="footer-area container-fluid">
-					<div className="row">
-						<div className="col-sm-6 col-md-4">
-							<h4>Steve Silvestri</h4>
-							<p>Software Engineer, JavaScript Developer</p>
-							<small>
-								&copy; Steven Silvestri 2017
-							</small>
-						</div>
-						<div className="col-sm-6 col-md-4">
-							<h4>Download CV</h4>
-							<p>
-								<a
-									rel="noopener noreferrer"
-									href="https://drive.google.com/open?id=0B-QjQxm8doHNeW0xUWVjWUE0V1E"
-									target="_blank"
-								>
-									<i className="fa fa-arrow-circle-o-down"/>&nbsp; Download
-								</a>
-							</p>
-						</div>
-						<div className="col-sm-6 col-md-4">
-							<h4>Follow me at</h4>
-							<ul className="SocialMedia">
-								<li className="social-icon">
-									<a href="https://www.github.com/ssilve1989">
-										<i className="fa fa-2x fa-github"/>
-									</a>
-								</li>
-								<li className="social-icon">
-									<a href="http://www.facebook.com/steve.silvestri.33">
-										<i className="fa fa-2x fa-facebook-square"/>
-									</a>
-								</li>
-								<li className="social-icon">
-									<a href="https://www.twitter.com/jsx1080">
-										<i className="fa fa-2x fa-twitter"/>
-									</a>
-								</li>
-								<li className="social-icon">
-									<a href="http://www.instagram.com/jsx1080">
-										<i className="fa fa-2x fa-instagram"/>
-									</a>
-								</li>
-								<li className="social-icon">
-									<a href="http://www.linkedin.com/in/stevensilvestri">
-										<i className="fa fa-2x fa-linkedin"/>
-									</a>
-								</li>
-								<li className="social-icon">
-									<a href="http://www.soundcloud.com/stevesilvestri">
-										<i className="fa fa-2x fa-soundcloud"/>
-									</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</footer>
+				<main className="RouteContainer">
+					<Resume/>
+				</main>
 			</div>
 		)
 	}
-
-	getTextScroller = () => (
-		<TextScroller fixed="Professional"
-		              values={ [
-			              'Software Engineer',
-			              'Front End Developer',
-			              'Java Developer',
-			              'JavaScript Engineer'
-		              ] }
-		/>
-	);
-
-	renderRoutes = () => (
-		<Switch>
-			<main className="RouteContainer">
-				<Route path="/" exact component={ Home }/>
-				<Route path="/resume" component={ Resume }/>
-				<Route path="/career" component={ Career }/>
-			</main>
-		</Switch>
-	)
 }
 
 export default App
