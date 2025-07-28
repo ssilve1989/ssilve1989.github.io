b84bdc46420d6b9c0619d9cf4103eb4e9f964f00
Steven Silvestri <steven.m.silvestri@gmail.com>
2025-07-28 14:13:48 -0600

    refactor: update astro dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ssilve1989.github.io',
   base: '/',
   integrations: [
-    tailwind(),
     sitemap()
   ],
   build: {
     assets: 'assets'
-  }
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
